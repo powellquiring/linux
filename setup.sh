5996ca25101938d47d37ace437a431ff393d6899
Powell Quiring <powellquiring@gmail.com>
2020-09-04 18:03:17 +0000

    changes

--- setup.sh
+++ setup.sh
@@ -5,3 +5,10 @@ cp .vimrc ~
 
 git config --global user.email "powellquiring@gmail.com"
 git config --global user.name "Powell Quiring"
+
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+echo 'pbcopy < ~/.ssh/id_rsa'
+cat > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+
