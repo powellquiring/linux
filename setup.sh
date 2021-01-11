2688c05d0c280cc0764861f4a32913b8547e8c27
Powell Quiring <powellquiring@gmail.com>
2021-01-11 22:50:12 +0000

    key protect

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-bash -x $DIR/setup2.sh
+$DIR/setup2.sh
 cat /tmp/.bashrc
 source /tmp/.bashrc
 cat /tmp/.bashrc >> ~/.bashrc
